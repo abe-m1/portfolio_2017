3a231abbb07965a610bc25d34fe122c824923250
abe <abestbet1@gmail.com>
2017-01-18 22:57:03 -0800

    rewrite user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,44 +1,83 @@
-//Bring in required packages
-var mongoose        = require( 'mongoose' )
-var bcrypt          = require( 'bcrypt-nodejs' )
-var Schema          = mongoose.Schema
+const mongoose  = require('mongoose')
+const bcrypt = require('bcrypt-nodejs')
 
+const Schema = mongoose.Schema
 
-//Set up schema
-var User = new Schema({
-
-  email: String,
-  username: ({ type: String, required: true, unique: true }),
-  password: ({ type: String, select: false })
+const userSchema = new Schema({
+    email: { type: String, unique: true, lowercase: true },
+    password: String
 })
 
-//Hash the password
-User.pre('save', function(next){
+userSchema.pre('save', function(next){
+    const user = this
+    bcrypt.genSalt(10, function(err, salt){
+        if (err) { return  next(err)}
 
-  var user = this
-  if (!user.isModified( 'password' )) {
-    return next()
-  } else {
+        bcrypt.hash(user.password, salt, null, function(err, hash){
+            if (err) { return next(err)}
 
-    //generate the salt
-    bcrypt.hash( user.password, null, null, function( err, hash ) {
-      if ( err ) {
-        return next( err )
-      } else {
+            user.password = hash
 
-        //change the password to the hashed version
-        user.password = hash
-        next()
-      }
+            next()
+        }) 
     })
-  }
 })
 
-//Method to compare entered password with that in DB
-User.methods.comparePassword = function( password ) {
-  var user = this
-  return bcrypt.compareSync( password, user.password )
+
+userSchema.methods.comparePassword = function(candidatePassword, callback){
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if (err) { return callback(err)}
+
+        callback(null, isMatch)
+    })
 }
 
-//Export the model
-module.exports = mongoose.model( 'User', User )
+const ModelClass = mongoose.model('user', userSchema)
+module.exports = ModelClass
+
+
+
+// //Bring in required packages
+// var mongoose        = require( 'mongoose' )
+// var bcrypt          = require( 'bcrypt-nodejs' )
+// var Schema          = mongoose.Schema
+
+
+// //Set up schema
+// var User = new Schema({
+
+//   email: String,
+//   username: ({ type: String, required: true, unique: true }),
+//   password: ({ type: String, select: false })
+// })
+
+// //Hash the password
+// User.pre('save', function(next){
+
+//   var user = this
+//   if (!user.isModified( 'password' )) {
+//     return next()
+//   } else {
+
+//     //generate the salt
+//     bcrypt.hash( user.password, null, null, function( err, hash ) {
+//       if ( err ) {
+//         return next( err )
+//       } else {
+
+//         //change the password to the hashed version
+//         user.password = hash
+//         next()
+//       }
+//     })
+//   }
+// })
+
+// //Method to compare entered password with that in DB
+// User.methods.comparePassword = function( password ) {
+//   var user = this
+//   return bcrypt.compareSync( password, user.password )
+// }
+
+// //Export the model
+// module.exports = mongoose.model( 'User', User )

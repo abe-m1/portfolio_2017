e8b9dc902469ef8776036c9aef8b0083bcf02082
abe <abestbet1@gmail.com>
2017-01-22 10:34:36 -0800

    connect app.modules.js

---
+++ public/app/core/modules/app.modules.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict'
+
+  angular
+    .module('MyProtfolio', ['MyPortfolio.Controllers',
+      'MyPortfolio.Services',
+      'MyPortfolio.Configs',
+      'MyPortfolio.Directives',
+      'MyPortfolio.Constants',
+      'MyPortfolio.Filters',
+      'ui.router'
+    ])
+
+  .run(function($rootScope) {
+    $rootScope.$on('$stateChangeStart', function(evt, toState, toParams, fromState, fromParams, $state) {
+      // console.log('Traveling to ' + toState.url, toState);
+      // $rootScope.contentTitle = ;
+
+
+      $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+        event.preventDefault();
+        //   console.log("your state error", error)
+      })
+    })
+  })
+
+})();

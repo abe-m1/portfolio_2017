222777e8b216429ce4b8de4df510cfe76dc6e561
abe <abestbet1@gmail.com>
2017-01-22 10:59:33 -0800

    create token service

---
+++ public/app/core/services/token.service.js
@@ -0,0 +1,33 @@
+(function (module){
+    var tokenService;
+    tokenService.$inject = ['localStorageManager'];
+    function tokenService(localStorageManager) {
+
+        var exports = {
+            store: store,
+            retrieve: retrieve,
+            decode: decode,
+            destroy: destroy
+        }
+
+        function store(key, token) {
+            return localStorageManager.store(key, token)
+        }
+
+        function retrieve(key){
+            return localStorageManager.retrieve(key)
+        }
+
+        function decode(key) {
+            return localStorageManager.decode(key)
+        }
+
+        function destroy(key){
+            localStorageManager.destroy(key)
+        }
+
+        return exports
+    }
+
+    module.factory("tokenService", tokenService)
+}(angular.module('MyPortfolio.Services')))

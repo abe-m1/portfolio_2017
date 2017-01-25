cc17fa9154f04c0cf0b04b1663ea6d68f2f49683
abe <abestbet1@gmail.com>
2017-01-24 22:07:03 -0800

    create token signing service

---
+++ public/app/core/services/tokenSigning.service.js
@@ -0,0 +1,21 @@
+(function(module) {
+  var tokenSigningService;
+  tokenSigningService.$inject = ['tokenService', '$log']
+
+  function tokenSigningService(tokenService, $log) {
+
+      function request(config){
+          if (tokenServcie.retrieve('portfolio_token')){
+              config.headers['x-portfolio-user-session-key'] = tokenService.retrieve('portfolio_token')
+          }
+
+          return config
+      }
+
+      return {
+          request: request
+      }
+
+  }
+  module.factory('tokenSigningService', tokenSigningService);
+}(angular.module("Yobs.Services")));
